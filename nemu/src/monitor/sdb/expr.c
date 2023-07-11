c88f8adeae8c3d7b20be9d39160e2decebc805ca
tracer-ics2022 <tracer@njuics.org>
2023-07-11 18:00:44 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:00:44 up 10:08,  3 users,  load average: 0.02, 0.02, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -22,7 +22,7 @@
 static word_t bra[20];
 static int bra_len=0;
 enum {
-  TK_NOTYPE = 256, TK_PLUS,TK_MI,TK_MUL,TK_EQ,TK_TNUMBER,
+  TK_NOTYPE = 256, TK_PLUS,TK_MI,TK_MUL,TK_EQ,TK_TNUMBER,TK_HEX,
 
   /* TODO: Add more token types */
 
@@ -43,6 +43,7 @@ static struct rule {
         {"-", '-'},
         {"\\*", '*'},
         {"[0-9]+", TK_TNUMBER},
+	{"0x[0-9]{8}",TK_HEX},
         {"\\(", '(',},
         {"\\)", ')'}, //
         {"==", TK_EQ},        // equal
