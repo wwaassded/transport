30c694ec60309d06f77c4f32d5e2c81fac939c2a
tracer-ics2022 <tracer@njuics.org>
2023-07-12 16:55:42 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:55:42 up  9:06,  3 users,  load average: 0.16, 0.14, 0.06

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -252,8 +252,11 @@ u_int32_t eval(Token pToken[], u_int32_t left, u_int32_t right, bool *success)
       }
       if (pToken[op_pos].type == 114514)
         return -1 * eval(pToken, op_pos + 1, right, success);
-      else if (pToken[op_pos].type == TK_DECODE)
+      else if (pToken[op_pos].type == TK_DECODE) {
+        uint32_t res = eval(pToken,op_pos+1,right,success);
+        printf("0x%x\n",res);
         return paddr_read(eval(pToken, op_pos + 1, right, success), 1);
+      }
       int right_number = eval(pToken, op_pos + 1, right, success);
       int left_number = eval(pToken, left, op_pos - 1, success);
       switch (pToken[op_pos].type)
