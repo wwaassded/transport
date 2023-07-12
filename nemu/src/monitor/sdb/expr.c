5f500e9427d749b6529260b57b139a87ffc8f949
tracer-ics2022 <tracer@njuics.org>
2023-07-12 10:39:22 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:39:22 up  2:49,  1 user,  load average: 0.19, 0.16, 0.06

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <isa.h>
 /* We use the POSIX regex functions to process regular expressions.
@@ -20,33 +20,45 @@
 #include <regex.h>
 
 static word_t bra[20];
-static int bra_len=0;
-enum {
-  TK_NOTYPE = 256, TK_PLUS,TK_MI,TK_MUL,TK_EQ,TK_TNUMBER,TK_HEX,
+static int bra_len = 0;
+enum
+{
+  TK_NOTYPE = 256,
+  TK_PLUS,
+  TK_MI,
+  TK_MUL,
+  TK_EQ,
+  TK_TNUMBER,
+  TK_HEX,
 
   /* TODO: Add more token types */
 
 };
 
-static struct rule {
-    const char *regex;
-    int token_type;
+static struct rule
+{
+  const char *regex;
+  int token_type;
 } rules[] = {
 
-        /* TODO: Add more rules.
-   * Pay attention to the precedence level of different rules.
-   */
+    /* TODO: Add more rules.
+     * Pay attention to the precedence level of different rules.
+     */
 
-        {" +", TK_NOTYPE},    // spaces
-        {"\\+", '+'},         // plus
-        {"/", '/'},
-        {"-", '-'},
-        {"\\*", '*'},
-        {"[0-9]+", TK_TNUMBER},
-	{"0x[0-9]{8}",TK_HEX},
-        {"\\(", '(',},
-        {"\\)", ')'}, //
-        {"==", TK_EQ},        // equal
+    {" +", TK_NOTYPE}, // spaces
+    {"\\+", '+'},      // plus
+    {"/", '/'},
+    {"-", '-'},
+    {"\\*", '*'},
+    {"[0-9]+", TK_TNUMBER},
+    {"0x[0-9]{8}", TK_HEX},
+    {
+        "\\(",
+        '(',
+    },
+    {"\\)", ')'},
+    {"\\$[a-z]{0,2}[1-9]{0,1}[0-9]{0,1}"},
+    {"==", TK_EQ},
 };
 
 #define NR_REGEX 9
@@ -56,162 +68,195 @@ static regex_t re[NR_REGEX] = {};
 /* Rules are used for many times.
  * Therefore we compile them only once before any usage.
  */
-void init_regex() {
+void init_regex()
+{
   int i;
   char error_msg[128];
   int ret;
 
-  for (i = 0; i < NR_REGEX; i ++) {
+  for (i = 0; i < NR_REGEX; i++)
+  {
     ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
-    if (ret != 0) {
+    if (ret != 0)
+    {
       regerror(ret, &re[i], error_msg, 128);
       panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
     }
   }
 }
 
-typedef struct token {
+typedef struct token
+{
   int type;
   char str[32];
 } Token;
 
 static Token tokens[32] __attribute__((used)) = {};
-static int nr_token __attribute__((used))  = 0;
+static int nr_token __attribute__((used)) = 0;
 
-static bool make_token(char *e) {
-    int position = 0;
-    int i;
-    regmatch_t pmatch;
-    nr_token = 0;
-    bra_len = 0;
-    u_int32_t fake_stack[100];
-    int pos = 0;
-    bool flag;
-    fake_stack[pos++] = 114514;
-    while (e[position] != '\0') {
-        /* Try all rules one by one. */
-        for (i = 0; i < NR_REGEX; ++i) {
-            if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
-                char *substr_start = e + position;
-                int substr_len = pmatch.rm_eo;
-                if(i != 0) {
-                    tokens[nr_token].type = rules[i].token_type;
-                    position += substr_len;
-                    strncpy(tokens[nr_token].str,substr_start,substr_len);
-                    if(substr_len < 32)
-                        tokens[nr_token].str[substr_len] = '\0';
-                    else
-                        tokens[nr_token].str[31] = '\0';
-                    ++nr_token;
-                }
-
-                switch (rules[i].token_type) {
-                    case '(': {
-                        fake_stack[pos++] = nr_token-1;
-                        break ;
-                    }
-                    case ')': {
-                        flag = fake_stack[--pos] != 114514;
-                        if(!flag)
-                            return false;
-                        bra[bra_len++] = fake_stack[pos];
-                        break ;
-                    }
-                }
-                break ;
-            }
-            if (i == NR_REGEX) {
-                printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
-                return false;
-            }
+static bool make_token(char *e)
+{
+  int position = 0;
+  int i;
+  regmatch_t pmatch;
+  nr_token = 0;
+  bra_len = 0;
+  u_int32_t fake_stack[100];
+  int pos = 0;
+  bool flag;
+  fake_stack[pos++] = 114514;
+  while (e[position] != '\0')
+  {
+    /* Try all rules one by one. */
+    for (i = 0; i < NR_REGEX; ++i)
+    {
+      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0)
+      {
+        char *substr_start = e + position;
+        int substr_len = pmatch.rm_eo;
+        if (i != 0)
+        {
+          Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
+          tokens[nr_token].type = rules[i].token_type;
+          position += substr_len;
+          strncpy(tokens[nr_token].str, substr_start, substr_len);
+          if (substr_len < 32)
+            tokens[nr_token].str[substr_len] = '\0';
+          else
+            tokens[nr_token].str[31] = '\0';
+          ++nr_token;
         }
-    }
-    if(pos != 1)
+
+        switch (rules[i].token_type)
+        {
+        case '(':
+        {
+          fake_stack[pos++] = nr_token - 1;
+          break;
+        }
+        case ')':
+        {
+          flag = fake_stack[--pos] != 114514;
+          if (!flag)
+            return false;
+          bra[bra_len++] = fake_stack[pos];
+          break;
+        }
+        }
+        break;
+      }
+      if (i == NR_REGEX)
+      {
+        printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
         return false;
-    return true;
+      }
+    }
+  }
+  if (pos != 1)
+    return false;
+  return true;
 }
 
-int eval(Token pToken[],u_int32_t left,u_int32_t right, bool *success) {
-    if(left > right) {
-        *success = false;
+int eval(Token pToken[], u_int32_t left, u_int32_t right, bool *success)
+{
+  if (left > right)
+  {
+    *success = false;
+    return -1;
+  }
+  else if (left == right)
+  {
+    return atoi(pToken[left].str);
+  }
+  else
+  {
+    if (pToken[left].type == '(' && pToken[right].type == ')' && bra[bra_len - 1] == left)
+    {
+      --bra_len;
+      return eval(pToken, left + 1, right - 1, success);
+    }
+    else
+    {
+      int is_in_bra = 0;
+      int op_pos = -100;
+      int op_tmp = left;
+      for (op_tmp = left; op_tmp <= right; ++op_tmp)
+      {
+        if (pToken[op_tmp].type == '(')
+          ++is_in_bra;
+        else if (pToken[op_tmp].type == ')')
+          --is_in_bra;
+        else if (is_in_bra == 0 && pToken[op_tmp].type != TK_TNUMBER)
+        {
+          if (pToken[op_tmp].type == '+' || pToken[op_tmp].type == '-')
+            op_pos = op_tmp;
+          else if (pToken[op_tmp].type == '*' || pToken[op_tmp].type == '/')
+          {
+            if (op_pos == -100 || pToken[op_pos].type == '*' || pToken[op_pos].type == '/' || pToken[op_pos].type == 114514)
+              op_pos = op_tmp;
+          }
+          else if (pToken[op_tmp].type == 114514)
+          {
+            if (op_pos == -100)
+              op_pos = op_tmp;
+          }
+        }
+      }
+      if (pToken[op_pos].type == 114514)
+        return -1 * eval(pToken, op_pos + 1, right, success);
+      int right_number = eval(pToken, op_pos + 1, right, success);
+      int left_number = eval(pToken, left, op_pos - 1, success);
+      switch (pToken[op_pos].type)
+      {
+      case '+':
+      {
+        return left_number + right_number;
+      }
+      case '-':
+      {
+        return left_number - right_number;
+      }
+      case '*':
+      {
+        return left_number * right_number;
+      }
+      case '/':
+      {
+        if (right_number == 0)
+        {
+          *success = false;
+          printf("a number can not devide zero!\n");
+          return -1;
+        }
+        return left_number / right_number;
+      }
+      default:
         return -1;
+      }
     }
-    else if(left == right) {
-        return atoi(pToken[left].str);
-    }
-    else {
-        if(pToken[left].type=='(' && pToken[right].type==')' && bra[bra_len-1]==left) {
-            --bra_len;
-            return eval(pToken,left+1,right-1,success);
-        }
-        else {
-            int is_in_bra = 0;
-            int op_pos = -100;
-            int op_tmp = left;
-            for(op_tmp = left; op_tmp<=right; ++op_tmp) {
-                if(pToken[op_tmp].type == '(')
-                    ++is_in_bra;
-                else if(pToken[op_tmp].type == ')')
-                    --is_in_bra;
-                else if(is_in_bra == 0 && pToken[op_tmp].type != TK_TNUMBER) {
-                    if(pToken[op_tmp].type=='+' || pToken[op_tmp].type=='-')
-                        op_pos = op_tmp;
-                    else if(pToken[op_tmp].type=='*' || pToken[op_tmp].type=='/') {
-			    if(op_pos == -100 || pToken[op_pos].type=='*' || pToken[op_pos].type=='/' || pToken[op_pos].type==114514)
-				    op_pos = op_tmp;
-		    }
-		    else if(pToken[op_tmp].type==114514) {
-			    if(op_pos == -100)
-				    op_pos = op_tmp;
-		    }
-                }
-            }
-	    if(pToken[op_pos].type == 114514)
-		    return -1 * eval(pToken,op_pos+1,right,success);
-            int right_number = eval(pToken,op_pos+1,right,success);
-            int left_number = eval(pToken,left,op_pos-1,success);
-            switch (pToken[op_pos].type) {
-                case '+': {
-                    return left_number + right_number;
-                }
-                case '-': {
-                    return left_number - right_number;
-                }
-                case '*': {
-                    return left_number * right_number;
-                }
-                case '/': {
-		    if(right_number == 0) {
-			    *success = false;
-			    printf("a number can not devide zero!\n");
-			    return -1;
-		    }
-                    return left_number / right_number;
-                }
-                default:
-                    return -1;
-            }
-        }
-    }
+  }
 }
 
-
-int expr(char *e, bool *success) {
-    if (!make_token(e)) {
-        *success = false;
-        return 0;
+int expr(char *e, bool *success)
+{
+  if (!make_token(e))
+  {
+    *success = false;
+    return 0;
+  }
+  else
+  {
+    int i = 0;
+    for (i = 0; i < nr_token; ++i)
+    {
+      if (tokens[i].type == '-')
+      {
+        if (i == 0)
+          tokens[i].type = 114514;
+        else if (tokens[i - 1].type != ')' && tokens[i - 1].type != TK_TNUMBER)
+          tokens[i].type = 114514;
+      }
     }
-    else {
-	    int i=0;
-	    for(i=0; i<nr_token; ++i) {
-		    if(tokens[i].type == '-'){
-			    if(i == 0)
-				    tokens[i].type = 114514;
-			    else if(tokens[i-1].type!=')' && tokens[i-1].type!=TK_TNUMBER)
-				    tokens[i].type = 114514;
-		    }
-	    }
-	    return eval(tokens,0,nr_token-1,success);
-    }
+    return eval(tokens, 0, nr_token - 1, success);
+  }
 }
-
