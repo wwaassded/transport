85b9d6321869a4aa6bae6025f1cc5b5b97725add
tracer-ics2022 <tracer@njuics.org>
2023-07-24 19:18:43 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:18:43 up 10:44,  1 user,  load average: 0.03, 0.07, 0.04

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -64,20 +64,20 @@ int strcmp(const char *s1, const char *s2)
 {
   assert(s1 && s2);
   do {
-    if(*s1 != *s2)
-      return *s1 - *s2;
-  } while (*s1++ || *s2++);
-  return 0;
+        if(*s1++ != *s2++)
+            return *(s1-1) - *(s2-1);
+    } while (*s1 || *s2);
+    return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n)
 {
   assert(s1 && s2);
   do {
-    if(*s1 != *s2)
-      return *s1 - *s2;
-  } while ((*s1++ || *s2++) && (--n!=0));
-  return 0;
+        if(*s1++ != *s2++)
+            return *(s1-1) - *(s2-1);
+    } while ((*s1 || *s2) && (--n!=0));
+    return 0;
 }
 
 void *memset(void *s, int c, size_t n)
