c18be4daeb9e3c1db986ca840ffb6cb1c53b59f2
tracer-ics2022 <tracer@njuics.org>
2023-07-24 19:38:41 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:38:41 up 11:04,  1 user,  load average: 0.11, 0.05, 0.01

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -82,7 +82,16 @@ int strncmp(const char *s1, const char *s2, size_t n)
 
 void *memset(void *s, int c, size_t n)
 {
-  panic("Not implemented");
+  if(s != nullptr) {
+        char *ptr = (char*)s;
+        while (n != 0)
+        {
+            *ptr = (unsigned int)c;
+            ++ptr;
+            --n;
+        }
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n)
@@ -97,7 +106,12 @@ void *memcpy(void *out, const void *in, size_t n)
 
 int memcmp(const void *s1, const void *s2, size_t n)
 {
-  panic("Not implemented");
+  assert(s1 && s2);
+  while(n) {
+        if((int8_t *)s1 != (int8_t *)s2) return *(int8_t *)s1 - *(int8_t *)s2;
+        --n;
+  }
+  return 0;
 }
 
 #endif
