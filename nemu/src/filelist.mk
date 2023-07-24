ed7cb61ab3bc44ca991005018de05aad55d277f2
tracer-ics2022 <tracer@njuics.org>
2023-07-24 09:52:58 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:52:58 up  1:18,  1 user,  load average: 0.04, 0.01, 0.00

--- nemu/src/filelist.mk
+++ nemu/src/filelist.mk
@@ -17,10 +17,8 @@ SRCS-y += src/nemu-main.c
 DIRS-y += src/cpu src/monitor src/utils
 DIRS-$(CONFIG_MODE_SYSTEM) += src/memory
 DIRS-BLACKLIST-$(CONFIG_TARGET_AM) += src/monitor/sdb
-@echo "fuck:$(CONFIG_TARGET_AM)"
 
-SHARE = $(if $(CONFIG_TARGET_SHARE),1,0)
-LIBS += $(if $(CONFIG_TARGET_NATIVE_ELF),-lreadline -ldl -pie,)
+
 
 ifdef mainargs
 ASFLAGS += -DBIN_PATH=\"$(mainargs)\"
