7bab2b9aa70fc611d1b69a38d7b0afecda7d5464
tracer-ics2022 <tracer@njuics.org>
2023-07-24 14:24:52 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:24:52 up  5:50,  1 user,  load average: 0.15, 0.10, 0.02

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -51,7 +51,7 @@ app: $(BINARY)
 
 $(BINARY): $(OBJS) $(ARCHIVES)
 	@echo + LD $@
-	@$(LD) -o $@ $(OBJS) $(LDFLAGS) $(ARCHIVES) $(LIBS)
+	@$(LD) -o $@ $(OBJS) $(LDFLAGS) $(ARCHIVES) $(LIBS) $(CFLAGS)
 
 clean:
 	-rm -rf $(BUILD_DIR)
--- nemu/src/filelist.mk
+++ nemu/src/filelist.mk
@@ -20,12 +20,8 @@ DIRS-BLACKLIST-$(CONFIG_TARGET_AM) += src/monitor/sdb
 
 SHARE = $(if $(CONFIG_TARGET_SHARE),1,0)
 LIBS += $(if $(CONFIG_TARGET_NATIVE_ELF),-lreadline -ldl -pie,)
-ifdef CONFIG_TARGET_AM
-	CFLAGS := haha
-endif
 ifdef mainargs
 ASFLAGS += -DBIN_PATH=\"$(mainargs)\"
-CFLAGS := haha
 endif
 SRCS-$(CONFIG_TARGET_AM) += src/am-bin.S
 .PHONY: src/am-bin.S
