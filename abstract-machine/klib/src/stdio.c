2e652d6cd343c2cf8b613d39def1cd9b7d64fa76
tracer-ics2022 <tracer@njuics.org>
2023-07-25 14:53:46 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:53:46 up 1 day,  6:19,  1 user,  load average: 0.12, 0.12, 0.07

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,23 +5,54 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
+#define nullptr NULL
+int printf(const char *fmt, ...)
+{
   panic("Not implemented");
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
+int vsprintf(char *out, const char *fmt, va_list ap)
+{
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
+int sprintf(char *out, const char *fmt, ...)
+{
+  assert(out != nullptr);
+  char *ptr = out;
+  va_list ap;
+  va_start(ap, fmt);
+  while (*fmt)
+  {
+
+    if(*fmt == '%') {
+      ++fmt;
+
+      switch(*fmt) {
+
+        case 's': {
+          const char *tmp = va_arg(ap,const char *);
+          while(*tmp)
+            *ptr++ = *tmp++;
+          break;
+        }
+        
+      }
+
+    }
+    ++fmt;
+  }
+  *ptr = '\0';
+  return ptr - out - 1;
+}
+
+int snprintf(char *out, size_t n, const char *fmt, ...)
+{
   panic("Not implemented");
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
+{
   panic("Not implemented");
 }
 
