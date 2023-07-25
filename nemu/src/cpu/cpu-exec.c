2e2a11f335f1bf12f2525a78c376492c7b733eb1
tracer-ics2022 <tracer@njuics.org>
2023-07-25 16:04:39 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:04:39 up 1 day,  7:30,  1 user,  load average: 0.23, 0.11, 0.03

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -33,6 +33,7 @@ static bool g_print_step = false;
 void device_update();
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
+  printf("fuck you bitch!\n");
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
 #endif
