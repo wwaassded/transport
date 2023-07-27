91cd7cf207ae39c03e73c6afe8518f8a38a31d96
tracer-ics2022 <tracer@njuics.org>
2023-07-27 11:07:19 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:07:19 up  2:03,  1 user,  load average: 0.22, 0.20, 0.13

--- nemu/src/utils/parse_elf.c
+++ nemu/src/utils/parse_elf.c
@@ -1,6 +1,41 @@
 #include <common.h>
 #include <elf.h>
+#include <sys/mman.h>
 
-void init_elf(const char *elf_file) {
-  printf("TEST::%s\n",elf_file);
+static FILE *fp;
+static void *elf = NULL;
+
+void parse_elf(const char *elf_file);
+
+void init_elf(const char *elf_file)
+{
+  assert(elf_file);
+  fp = fopen(elf_file, "r");
+  if (fp == NULL)
+    printf("can not open your elf_file!\n");
+  else
+  {
+    fseek(fp, 0, SEEK_END);
+    size_t number = ftell(fp);
+    elf = mmap(NULL, number, PROT_READ, MAP_PRIVATE, fileno(fp), 0);
+    fclose(fp);
+    if (elf == MAP_FAILED)
+      perror("in_init_elf");
+    else
+      parse_elf(elf_file);
+  }
+}
+
+
+void parse_elf(const char *elf_file)
+{
+  assert(elf);
+  Elf32_Ehdr elf_hdr;
+  memmove(&elf_hdr,elf,sizeof(Elf32_Ehdr));
+  unsigned char expected_magicnumber[] = {ELFMAG0,ELFMAG1,ELFMAG2,ELFMAG3};
+  if(memcmp(elf_hdr.e_ident,expected_magicnumber,sizeof(expected_magicnumber)) != 0) {
+    printf("%s is not a elf file please check your makefile!\n",elf_file);
+    return;
+  }
+  printf("Congratulation!\n");
 }
