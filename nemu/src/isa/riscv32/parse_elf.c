c83685b1eab17b77591b65c344b2e0de70d9c607
tracer-ics2022 <tracer@njuics.org>
2023-07-29 09:49:09 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:49:09 up 2 days, 45 min,  3 users,  load average: 0.03, 0.03, 0.04

--- nemu/src/isa/riscv32/parse_elf.c
+++ nemu/src/isa/riscv32/parse_elf.c
@@ -121,10 +121,9 @@ void parse_decode(Decode *s, vaddr_t pc) {
         }
         printf("%08x %08x\n", pc, s->dnpc);
         printf("%u %u\n", tar, ori);
-        if (tar != F_len + 1 && strcmp(func_info[abs(tar)].F_name, func_info[ori].F_name) != 0) {
+        if (tar != 0 && strcmp(func_info[tar].F_name, func_info[ori].F_name) != 0) {
             if (f_jal == 0) {
                 printf("FUCK!\n");
-                tar = -tar;
                 for (ii = 0; ii < number; ++ii)
                     fprintf(ftrace_fp, " ");
                 fprintf(ftrace_fp, "[0x%08x:call %s in %s]\n", pc, func_info[tar].F_name, func_info[ori].F_name);
