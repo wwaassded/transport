b6c600e1249f28a87c3738f852124cac2990cbc7
tracer-ics2022 <tracer@njuics.org>
2023-07-31 16:19:17 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:19:17 up  7:38,  1 user,  load average: 0.06, 0.03, 0.00

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
 
 extern char _heap_start;
 int main(const char *args);
@@ -11,7 +12,8 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
-  outb(SERIAL_PORT, ch);
+    printf("FUCK_Q!\n");
+    outb(SERIAL_PORT, ch);
 }
 
 void halt(int code) {
