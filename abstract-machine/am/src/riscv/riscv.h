f66f61036b935f6f87df1c089b390f0f85fd096e
tracer-ics2022 <tracer@njuics.org>
2023-07-31 16:13:36 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:13:36 up  7:33,  1 user,  load average: 0.17, 0.04, 0.01

--- abstract-machine/am/src/riscv/riscv.h
+++ abstract-machine/am/src/riscv/riscv.h
@@ -9,7 +9,7 @@ static inline uint32_t inl(uintptr_t addr) { return *(volatile uint32_t *)addr; 
 
 static inline void outb(uintptr_t addr, uint8_t  data) {
     
-    // *(volatile uint8_t *) addr = data;
+    *(volatile uint8_t *) addr = data;
 }
 static inline void outw(uintptr_t addr, uint16_t data) { *(volatile uint16_t *)addr = data; }
 static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)addr = data; }
