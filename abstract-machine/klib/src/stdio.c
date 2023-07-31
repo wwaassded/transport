a503bca758f6c1159530f0d862dc04691239a097
tracer-ics2022 <tracer@njuics.org>
2023-07-31 10:34:21 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:34:21 up  1:53,  1 user,  load average: 0.07, 0.10, 0.07

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,87 +1,119 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 #define nullptr NULL
 static char buf[32];
-int printf(const char *fmt, ...)
-{
-  panic("Not implemented");
-}
-
-int vsprintf(char *out, const char *fmt, va_list ap)
-{
-  panic("Not implemented");
-}
-
-int sprintf(char *out, const char *fmt, ...)
-{
-  assert(out != nullptr);
-  char *ptr = out;
-  va_list ap;
-  va_start(ap, fmt);
-  while (*fmt)
-  {
-
-    if (*fmt == '%')
-    {
-      ++fmt;
-
-      switch (*fmt)
-      {
-
-      case 's':
-      {
-        const char *tmp = va_arg(ap, const char *);
-        while (*tmp)
-          *ptr++ = *tmp++;
-        break;
-      }
-
-      case 'd':
-      {
-        int tmp = va_arg(ap, int);
-        uint8_t i;
-        if (tmp < 0)
-        {
-          *ptr++ = '-';
-          tmp = -tmp;
+int printf(const char *fmt, ...) {
+    uint32_t res = 0;
+    va_list ap;
+    va_start(ap, fmt);
+    while (*fmt != '\n') {
+        if (*fmt == '%') {
+            fmt = fmt + 1;
+            switch (*fmt) {
+                case 's': {
+                    const char *str = va_arg(ap, const char *);
+                    putstr(str);
+                    res += strlen(str);
+                    break;
+                }
+                case 'd': {
+                    uint8_t i;
+                    int num = va_arg(ap, int);
+                    if (num < 0) {
+                        putch('-');
+                        num = -num;
+                    }
+                    for (i = 0; num != 0; ++i) {
+                        buf[i] = (num % 10) + '0';
+                        num /= 10;
+                    }
+                    res += i + 1;
+                    --i;
+                    while (i != 0) {
+                        putch(buf[i]);
+                        --i;
+                    }
+                    putch(buf[i]);
+                    break;
+                    break;
+                }
+                default: {
+                    panic("Not implemented");
+                }
+            }
+        } else {
+            ++res;
+            putch(*fmt);
         }
-        for (i = 0; tmp != 0; ++i)
-        {
-          buf[i] = (tmp % 10) + '0';
-          tmp /= 10;
-        }
-        --i;
-        while (i != 0)
-        {
-          *ptr++ = buf[i];
-          --i;
-        }
-        *ptr++ = buf[i];
-        break;
-      }
-      }
+        fmt = fmt + 1;
     }
-    else
-      *ptr++ = *fmt;
-    ++fmt;
-  }
-  *ptr = '\0';
-  return ptr - out - 1;
+    va_end(ap);
+    fmt = fmt + 1;
+    return res;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...)
-{
-  panic("Not implemented");
+int vsprintf(char *out, const char *fmt, va_list ap) {
+    panic("Not implemented");
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
-{
-  panic("Not implemented");
+int sprintf(char *out, const char *fmt, ...) {
+    assert(out != nullptr);
+    char *ptr = out;
+    va_list ap;
+    va_start(ap, fmt);
+    while (*fmt) {
+
+        if (*fmt == '%') {
+            ++fmt;
+
+            switch (*fmt) {
+
+                case 's': {
+                    const char *tmp = va_arg(ap, const char *);
+                    while (*tmp)
+                        *ptr++ = *tmp++;
+                    break;
+                }
+
+                case 'd': {
+                    int tmp = va_arg(ap, int);
+                    uint8_t i;
+                    if (tmp < 0) {
+                        *ptr++ = '-';
+                        tmp = -tmp;
+                    }
+                    for (i = 0; tmp != 0; ++i) {
+                        buf[i] = (tmp % 10) + '0';
+                        tmp /= 10;
+                    }
+                    --i;
+                    while (i != 0) {
+                        *ptr++ = buf[i];
+                        --i;
+                    }
+                    *ptr++ = buf[i];
+                    break;
+                }
+            }
+        } else
+            *ptr++ = *fmt;
+        ++fmt;
+    }
+    *ptr = '\0';
+    return ptr - out - 1;
+}
+
+int snprintf(char *out, size_t n, const char *fmt, ...) {
+    panic("Not implemented");
+}
+
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+    panic("Not implemented");
 }
 
 #endif
