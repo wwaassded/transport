dc680223a07a2ae8f398a802d2b45e1501affdb0
tracer-ics2022 <tracer@njuics.org>
2023-08-01 09:37:46 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:37:46 up 1 day, 57 min,  1 user,  load average: 0.17, 0.13, 0.22

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -26,7 +26,7 @@ static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
         printf("FUCK ");
         uint64_t us = get_time();
         rtc_port_base[0] = (uint32_t) us;
-        // rtc_port_base[1] = (uint32_t)(us >> 32);
+        rtc_port_base[1] = (us >> 32);
     }
 }
 
