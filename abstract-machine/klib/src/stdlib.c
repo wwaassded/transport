0d80be415aacec055676e2c64b697b3e504210d8
tracer-ics2022 <tracer@njuics.org>
2023-08-01 16:47:18 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:47:18 up 1 day,  8:06,  1 user,  load average: 0.20, 0.09, 0.09

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -1,42 +1,43 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
-
+static void *addr = NULL;
+bool flag = false;
 int rand(void) {
-  // RAND_MAX assumed to be 32767
-  next = next * 1103515245 + 12345;
-  return (unsigned int)(next/65536) % 32768;
+    // RAND_MAX assumed to be 32767
+    next = next * 1103515245 + 12345;
+    return (unsigned int) (next / 65536) % 32768;
 }
 
 void srand(unsigned int seed) {
-  next = seed;
+    next = seed;
 }
 
 int abs(int x) {
-  return (x < 0 ? -x : x);
+    return (x < 0 ? -x : x);
 }
 
-int atoi(const char* nptr) {
-  int x = 0;
-  while (*nptr == ' ') { nptr ++; }
-  while (*nptr >= '0' && *nptr <= '9') {
-    x = x * 10 + *nptr - '0';
-    nptr ++;
-  }
-  return x;
+int atoi(const char *nptr) {
+    int x = 0;
+    while (*nptr == ' ') { nptr++; }
+    while (*nptr >= '0' && *nptr <= '9') {
+        x = x * 10 + *nptr - '0';
+        nptr++;
+    }
+    return x;
 }
 
 void *malloc(size_t size) {
-  // On native, malloc() will be called during initializaion of C runtime.
-  // Therefore do not call panic() here, else it will yield a dead recursion:
-  //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
-#endif
-  return NULL;
+    if (!flag) {
+        flag = true;
+        addr = heap.start;
+    }
+    void *ptr = addr;
+    addr += size;
+    return ptr;
 }
 
 void free(void *ptr) {
