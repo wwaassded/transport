142659e930842548fcfaf15758b0c0896fe864cb
tracer-ics2022 <tracer@njuics.org>
2023-08-03 09:28:01 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:28:01 up 3 days, 47 min,  1 user,  load average: 0.01, 0.15, 0.10

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -32,7 +32,6 @@ void init_alarm();
 
 void send_key(uint8_t, bool);
 void vga_update_screen();
-void update_screen();
 
 void device_update() {
     static uint64_t last = 0;
@@ -43,7 +42,6 @@ void device_update() {
     last = now;
 
     IFDEF(CONFIG_HAS_VGA, vga_update_screen());
-    IFDEF(CONFIG_HAS_VGA, update_screen());
 
 #ifndef CONFIG_TARGET_AM
     SDL_Event event;
--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -72,6 +72,10 @@ static inline void update_screen() {
 #endif
 
 void vga_update_screen() {
+    SDL_UpdateTexture(texture, NULL, vmem, SCREEN_W * sizeof(uint32_t));
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
 }
 
 void init_vga() {
