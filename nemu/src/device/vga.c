0caf2f44da650192c04e20745c57aab537fbd5ce
tracer-ics2022 <tracer@njuics.org>
2023-08-03 10:01:05 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:01:05 up 3 days,  1:20,  1 user,  load average: 0.18, 0.14, 0.10

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -53,7 +53,7 @@ static void init_screen() {
             0, &window, &renderer);
     SDL_SetWindowTitle(window, title);
     texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
-                                SDL_TEXTUREACCESS_STATIC, 1, 1);
+                                SDL_TEXTUREACCESS_STATIC, 800, 600);
 }
 
 static inline void update_screen() {
