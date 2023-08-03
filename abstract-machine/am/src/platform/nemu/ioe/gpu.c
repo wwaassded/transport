b6b8791c2d5be512f1ba0813e5098c5436262d78
tracer-ics2022 <tracer@njuics.org>
2023-08-03 08:45:28 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:45:28 up 3 days, 4 min,  1 user,  load average: 0.17, 0.14, 0.09

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,13 +4,14 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+    printf("FUCK!\n");
     int i;
     uint32_t screen_config = inl(VGACTL_ADDR + 0);
     int w = screen_config >> 16;
     int h = screen_config & (0x0000ffff);
     uint32_t *fb = (uint32_t *) (uintptr_t) FB_ADDR;
     for (i = 0; i < w * h; i++) fb[i] = i;
-    outl(SYNC_ADDR, 0x0000ff00);
+    outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
