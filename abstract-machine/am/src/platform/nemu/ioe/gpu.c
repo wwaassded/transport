c9da15a5ccf972b1d85c70f679efb8dde9252010
tracer-ics2022 <tracer@njuics.org>
2023-08-03 08:12:08 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:12:08 up 2 days, 23:31,  1 user,  load average: 0.16, 0.15, 0.07

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,8 +5,9 @@
 
 void __am_gpu_init() {
     int i;
-    int w = 0;// TODO: get the correct width
-    int h = 0;// TODO: get the correct height
+    uint32_t screen_config = inl(VGACTL_ADDR + 0);
+    int w = screen_config >> 16;
+    int h = screen_config & (0x0000ffff);
     uint32_t *fb = (uint32_t *) (uintptr_t) FB_ADDR;
     for (i = 0; i < w * h; i++) fb[i] = i;
     outl(SYNC_ADDR, 1);
@@ -14,16 +15,12 @@ void __am_gpu_init() {
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
     uint32_t screen_config = inl(VGACTL_ADDR + 0);
-    printf("ALL:0x%08x\n",screen_config);
     *cfg = (AM_GPU_CONFIG_T){
             .present = true,
             .has_accel = false,
             .width = screen_config >> 16,
             .height = screen_config & (0x0000ffff),
             .vmemsz = 0};
-    if (cfg->height != (screen_config - (screen_config >> 16))) {
-        printf("FAG:0x%08x  0x%08x\n",cfg->width,cfg->height);
-    }
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
