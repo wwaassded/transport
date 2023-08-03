cfa8dbf17e52b7fa831a31e18b8be77c3be404c5
tracer-ics2022 <tracer@njuics.org>
2023-08-03 08:07:52 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:07:52 up 2 days, 23:27,  1 user,  load average: 0.20, 0.12, 0.04

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,6 +1,6 @@
 #include <am.h>
 #include <nemu.h>
-
+#include <stdio.h>
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
@@ -15,11 +15,15 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
     uint32_t screen_config = inl(VGACTL_ADDR + 0);
     *cfg = (AM_GPU_CONFIG_T){
-            .present = true,
-            .has_accel = false,
-            .width = screen_config >> 16,
-            .height = screen_config & (0x0000ffff),
-            .vmemsz = 0};
+        .present = true,
+        .has_accel = false,
+        .width = screen_config >> 16,
+        .height = screen_config & (0x0000ffff),
+        .vmemsz = 0
+    };
+    if (cfg->height != (screen_config - (screen_config >> 16))) {
+        printf("PANIC!\n");
+    }
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
