f5da9735a4a81eb19b5d867c121b68d8dae3aad9
tracer-ics2022 <tracer@njuics.org>
2023-08-03 08:10:08 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:10:08 up 2 days, 23:29,  1 user,  load average: 0.30, 0.17, 0.07

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -14,15 +14,15 @@ void __am_gpu_init() {
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
     uint32_t screen_config = inl(VGACTL_ADDR + 0);
+    printf("ALL:0x%08x\n",screen_config);
     *cfg = (AM_GPU_CONFIG_T){
-        .present = true,
-        .has_accel = false,
-        .width = screen_config >> 16,
-        .height = screen_config & (0x0000ffff),
-        .vmemsz = 0
-    };
+            .present = true,
+            .has_accel = false,
+            .width = screen_config >> 16,
+            .height = screen_config & (0x0000ffff),
+            .vmemsz = 0};
     if (cfg->height != (screen_config - (screen_config >> 16))) {
-        printf("PANIC!\n");
+        printf("FAG:0x%08x  0x%08x\n",cfg->width,cfg->height);
     }
 }
 
