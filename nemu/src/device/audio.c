c0f95274dd4b2504a92d9b3fcffa751cce0d1769
tracer-ics2022 <tracer@njuics.org>
2023-08-07 03:29:02 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:29:02 up 21:14,  1 user,  load average: 0.09, 0.12, 0.10

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -44,8 +44,9 @@ static void audio_play(void *userdata, uint8_t *stream, int len) {
     int nwrite = len;
     if (count < nwrite)
         nwrite = count;
-    printf("TTT::%d %d\n", full_len, left);
+
     for (int i = 0; i < nwrite; ++i) {
+        printf("TTT::%d %d\n", full_len, left);
         stream[i] = sbuf[left];
         left = (left + 1) % full_len;
     }
