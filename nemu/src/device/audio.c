8d6ea80184119dc64ff282b0c715e2bcac48db49
tracer-ics2022 <tracer@njuics.org>
2023-08-06 10:11:32 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:11:32 up  3:57,  1 user,  load average: 0.34, 0.09, 0.03

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -13,35 +13,60 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
+#include <SDL2/SDL.h>
 #include <common.h>
 #include <device/map.h>
-#include <SDL2/SDL.h>
 
 enum {
-  reg_freq,
-  reg_channels,
-  reg_samples,
-  reg_sbuf_size,
-  reg_init,
-  reg_count,
-  nr_reg
+    reg_freq,
+    reg_channels,
+    reg_samples,
+    reg_sbuf_size,
+    reg_init,
+    reg_count,
+    nr_reg
 };
 
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
-
+static SDL_AudioSpec desired;
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+    if (is_write) {
+        switch (offset) {
+            case 0: {
+                desired.freq = audio_base[0];
+                break;
+            }
+            case 4: {
+                desired.channels = audio_base[1];
+                break;
+            }
+            case 8: {
+                desired.samples = audio_base[2];
+                break;
+            }
+            default: {
+                panic("not implemented!\n");
+            }
+        }
+    } else {
+        switch (offset) {
+            default: {
+                panic("not implemented!\n");
+            }
+        }
+    }
 }
 
 void init_audio() {
-  uint32_t space_size = sizeof(uint32_t) * nr_reg;
-  audio_base = (uint32_t *)new_space(space_size);
+    uint32_t space_size = sizeof(uint32_t) * nr_reg;
+    audio_base = (uint32_t *) new_space(space_size);
 #ifdef CONFIG_HAS_PORT_IO
-  add_pio_map ("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size, audio_io_handler);
+    add_pio_map("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size, audio_io_handler);
 #else
-  add_mmio_map("audio", CONFIG_AUDIO_CTL_MMIO, audio_base, space_size, audio_io_handler);
+    add_mmio_map("audio", CONFIG_AUDIO_CTL_MMIO, audio_base, space_size, audio_io_handler);
 #endif
 
-  sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
-  add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
+    sbuf = (uint8_t *) new_space(CONFIG_SB_SIZE);
+    add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
 }
