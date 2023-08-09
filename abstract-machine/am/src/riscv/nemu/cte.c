16859e966f445e71e8a0503ebed9c957ffd72e65
tracer-ics2022 <tracer@njuics.org>
2023-08-09 10:57:18 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:57:18 up  7:49,  1 user,  load average: 0.03, 0.08, 0.08

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,7 +9,7 @@ Context *__am_irq_handle(Context *c) {
         Event ev = {0};
         switch (c->mcause) {
             default:
-                printf("%d\n", c->mcause);
+                printf("%s\n", *(char *)c->mcause);
                 ev.event = EVENT_ERROR;
                 break;
         }
