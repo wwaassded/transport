204d543c94acffdb71f28cf9e31f526c4ed3c8a2
tracer-ics2022 <tracer@njuics.org>
2023-08-09 09:49:51 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:49:51 up  6:41,  1 user,  load average: 0.00, 0.02, 0.07

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,7 +1,8 @@
 #include <common.h>
 
 static Context *do_event(Event e, Context *c) {
-    // yield();
+    printf("i am running!\n");
+    yield();
     switch (e.event) {
         default:
             panic("Unhandled event ID = %d", e.event);
