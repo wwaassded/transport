281132687ddba5f1da2d53b2242dc7f6f36c3a30
tracer-ics2022 <tracer@njuics.org>
2023-08-14 11:05:19 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:05:19 up  2:43,  1 user,  load average: 0.29, 0.26, 0.13

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,13 +11,17 @@ extern size_t get_ramdisk_size();
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 extern void init_ramdisk();
-
+// unsigned char expected_magicnumber[] = {ELFMAG0, ELFMAG1, ELFMAG2, ELFMAG3};
+// if (memcmp(elf_hdr.e_ident, expected_magicnumber, sizeof(expected_magicnumber)) != 0) {
+//     printf("%s is not a elf file please check your makefile!\n", elf_file);
+//     return;
+// }
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
     Elf_Ehdr ELF_head;
     ramdisk_read(&ELF_head, 0, sizeof(Elf_Ehdr));
-    unsigned char magic_number[] = {0x7f, 'E', 'L', 'F'};
-    if (memcpy(ELF_head.e_ident, magic_number, 4) != 0)
+    unsigned char magic_number[] = {ELFMAG0, ELFMAG1, ELFMAG2, ELFMAG3};
+    if (memcmp(ELF_head.e_ident, magic_number, sizeof(magic_number)) != 0)
         panic("it is not a elf file!");
     panic("YEE!");
 }
