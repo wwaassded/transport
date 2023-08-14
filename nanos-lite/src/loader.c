bf6619fd0319b51582a4aea3007819946438ebba
tracer-ics2022 <tracer@njuics.org>
2023-08-14 11:02:54 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:02:54 up  2:40,  1 user,  load average: 0.58, 0.29, 0.13

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -8,10 +8,18 @@
 #define Elf_Phdr Elf32_Phdr
 #endif
 extern size_t get_ramdisk_size();
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+extern void init_ramdisk();
+
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-    printf("FFF::%d\n", get_ramdisk_size());
-    panic("HERE!");
-    return 0;
+    Elf_Ehdr ELF_head;
+    ramdisk_read(&ELF_head, 0, sizeof(Elf_Ehdr));
+    unsigned char magic_number[] = {0x7f, 'E', 'L', 'F'};
+    if (memcpy(ELF_head.e_ident, magic_number, 4) != 0)
+        panic("it is not a elf file!");
+    panic("YEE!");
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
