19fac08c398a68b19b3a49a0ec99651b71aec84e
tracer-ics2022 <tracer@njuics.org>
2023-08-15 11:01:27 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:01:27 up  2:19,  1 user,  load average: 0.01, 0.06, 0.04

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,10 +5,12 @@ extern void do_syscall(Context *c);
 static Context *do_event(Event e, Context *c) {
     switch (e.event) {
         case EVENT_YIELD: {
+            printf("%d\n", c->mcause);
             printf("FUCK it is yield\n");
             break;
         }
         case EVENT_SYSCALL: {
+            printf("%d\n", c->mcause);
             do_syscall(c);
             break;
         }
