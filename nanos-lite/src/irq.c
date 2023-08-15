cb1207a3e198bb9e717b9dff1a604d4476784d14
tracer-ics2022 <tracer@njuics.org>
2023-08-15 15:47:03 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:47:03 up 19 min,  1 user,  load average: 0.07, 0.06, 0.06

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,12 +5,12 @@ extern void do_syscall(Context *c);
 static Context *do_event(Event e, Context *c) {
     switch (e.event) {
         case EVENT_YIELD: {
-            printf("8:%d\n", c->mcause);
+            // printf("8:%d\n", c->mcause);
             printf("FUCK it is yield\n");
             break;
         }
         case EVENT_SYSCALL: {
-            printf("13:%d\n", c->mcause);
+            // printf("13:%d\n", c->mcause);
             do_syscall(c);
             break;
         }
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,14 +1,30 @@
 #include "syscall.h"
-#include <common.h>
+
+
+#define SYS_yeild 1
+
+extern void yield();
 
 
 void do_syscall(Context *c) {
     uintptr_t a[4];
-    a[0] = 1;
-    printf("7::%d\n", c->gpr[11]);
+    a[0] = c->GPR1;
+    a[1] = c->GPR2;
+    a[2] = c->GPR3;
+    a[3] = c->GPR4;
     switch (a[0]) {
+        case SYS_yeild: {
+            sys_yield(c);
+            break;
+        }
         default:
             printf("TEST::FUCK::it is me !");
             panic("Unhandled syscall ID = %d", a[0]);
     }
+}
+
+
+void sys_yield(Context *c) {
+    yield();
+    c->GPRx = 0;
 }
