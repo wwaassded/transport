bc79690bb19b1d4c1cbf6a1c58929af3c22ed531
tracer-ics2022 <tracer@njuics.org>
2023-08-15 09:36:28 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:36:28 up 54 min,  1 user,  load average: 0.32, 0.14, 0.04

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -8,6 +8,5 @@
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-    printf("Hello I am Dummy!\n");
-    return _syscall_(SYS_yield, 0, 0, 0);
+    return _syscall_(2, 0, 0, 0);
 }
