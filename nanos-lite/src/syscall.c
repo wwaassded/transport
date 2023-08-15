5f74dc9237c0323e677f64410b7d62ebb0516db6
tracer-ics2022 <tracer@njuics.org>
2023-08-15 11:10:17 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:10:17 up  2:28,  1 user,  load average: 0.05, 0.05, 0.05

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,7 +5,7 @@
 void do_syscall(Context *c) {
     uintptr_t a[4];
     a[0] = 1;
-    printf("7::%d\n", c->gpr[10]);
+    printf("7::%d\n", c->gpr[11]);
     switch (a[0]) {
         default:
             printf("TEST::FUCK::it is me !");
