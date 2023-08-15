4897e4053d8acbd5b7e417ed52b6500be1457a20
tracer-ics2022 <tracer@njuics.org>
2023-08-15 10:06:59 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:06:59 up  1:24,  1 user,  load average: 0.17, 0.09, 0.04

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,10 +8,6 @@ Context *__am_irq_handle(Context *c) {
     if (user_handler) {
         Event ev = {0};
         switch (c->mcause) {
-            // case 0: {
-            //     ev.event = EVENT_YIELD;
-            //     break;
-            // }
             default:
                 printf("%d\n", c->mcause);
                 ev.event = EVENT_ERROR;
--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -20,7 +20,6 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    * Then return the address of the interrupt/exception vector.
    */
     csr.cgpr[0] = cpu.pc;
-    printf("%ld\n", csr.cgpr[2]);
     csr.cgpr[2] = NO;
     uint64_t new_pc = csr.cgpr[3];
     return new_pc;
