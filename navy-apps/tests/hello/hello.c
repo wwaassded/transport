f7e0b448123057ac2b2eb1afa50fef90426dc53b
tracer-ics2022 <tracer@njuics.org>
2023-08-16 15:23:07 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:23:07 up  5:23,  1 user,  load average: 0.22, 0.15, 0.17

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -4,7 +4,7 @@
 int main() {
     write(1, "Hello World!\n", 13);
     int number = 2;
-    for (int i = number; i < 100; ++i)
-        printf("FUCK:%d\n", i);
+    for (; number < 100; ++number)
+        printf("FUCK:%d\n", number);
     return 0;
 }
