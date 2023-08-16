60528c6c165e411a9a41af3639511bf8fb9a0fa6
tracer-ics2022 <tracer@njuics.org>
2023-08-16 10:50:59 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:50:59 up 51 min,  1 user,  load average: 1.60, 0.64, 0.27

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 
 extern char _end;
-static void *pro_bre = &(_end);
+static void *pro_bre = NULL;
 
 // helper macros
 #define _concat(x, y) x##y
@@ -74,12 +74,14 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {
-    if (increment == 0)
+    if (increment == 0) {
+        pro_bre = &(_end);
         return pro_bre;
+    }
     if (_syscall_(SYS_brk, (intptr_t) (pro_bre + increment), 0, 0) == 0) {
         void *ret_ptr = pro_bre;
         pro_bre = pro_bre + increment;
-        return pro_bre;
+        return ret_ptr;
     } else
         return (void *) -1;
 }
