7b7b5baa8db9de9de30d3fc5b3019e02b66fe153
what <2994899015@qq.com>
2023-08-16 14:27:22 +0800

    NJU-ProjectN/navy-apps ics2021 initialized
    NJU-ProjectN/navy-apps e2d345090b759898403aaeefe96bd46c580efeaf Merge pull request #5 from unlsycn/master

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -1,5 +1,5 @@
 #include <stdint.h>
-#include <stdio.h>
+
 #ifdef __ISA_NATIVE__
 #error can not support ISA=native
 #endif
@@ -8,5 +8,5 @@
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-    return _syscall_(SYS_yield, 0, 0, 0);
+  return _syscall_(SYS_yield, 0, 0, 0);
 }
