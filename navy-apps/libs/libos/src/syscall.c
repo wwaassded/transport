a6bd671c3823a55c920d17b0fa3f0f2aa4d84597
tracer-ics2022 <tracer@njuics.org>
2023-08-16 10:47:19 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:47:19 up 47 min,  1 user,  load average: 0.18, 0.19, 0.09

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -75,6 +75,8 @@ int _write(int fd, void *buf, size_t count) {
 
 void *_sbrk(intptr_t increment) {
     if (_syscall_(SYS_brk, (intptr_t) (pro_bre + increment), 0, 0) == 0) {
+        char res[] = {'a', 'b', 's'};
+        _write(1, res, sizeof(res));
         void *ret_ptr = pro_bre;
         pro_bre = pro_bre + increment;
         return ret_ptr;
