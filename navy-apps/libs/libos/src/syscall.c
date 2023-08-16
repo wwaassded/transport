7ce12abdf7d5301281e5e4494f0c8d9484dca437
tracer-ics2022 <tracer@njuics.org>
2023-08-16 15:12:12 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:12:12 up  5:12,  1 user,  load average: 0.19, 0.21, 0.23

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -69,7 +69,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-    // _exit(_syscall_(SYS_write, fd, (intptr_t) (buf), count));
+    _exit(_syscall_(SYS_write, fd, (intptr_t) (buf), count));
     _syscall_(SYS_write, fd, (intptr_t) (buf), count);
     return 0;
 }
