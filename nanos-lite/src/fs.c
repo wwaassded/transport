c6dc814a0381f94d0ed9923ba5f701461351c695
tracer-ics2022 <tracer@njuics.org>
2023-08-17 14:48:22 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:48:22 up 26 min,  1 user,  load average: 0.30, 0.14, 0.10

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -42,6 +42,7 @@ void init_fs() {
 }
 
 
+
 int fs_open(const char *pathname, int flags, int mode) {
     for (int i = 0; file_table[i].name != NULL; ++i)
         if (strcmp(file_table[i].name, pathname) == 0)
