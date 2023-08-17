32cd86908545f88b7d5d53f3407a72394893eccb
tracer-ics2022 <tracer@njuics.org>
2023-08-17 21:37:51 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:37:51 up  7:16,  1 user,  load average: 0.14, 0.08, 0.03

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -1,40 +1,39 @@
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
 
 int main() {
-  FILE *fp = fopen("/share/files/num", "r+");
-  assert(fp);
+    FILE *fp = fopen("/share/files/num", "r+");
+    assert(fp);
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    assert(size == 5000);
+    return 0;
+    fseek(fp, 500 * 5, SEEK_SET);
+    int i, n;
+    for (i = 500; i < 1000; i++) {
+        fscanf(fp, "%d", &n);
+        assert(n == i + 1);
+    }
 
-  fseek(fp, 0, SEEK_END);
-  long size = ftell(fp);
-  assert(size == 5000);
+    fseek(fp, 0, SEEK_SET);
+    for (i = 0; i < 500; i++) {
+        fprintf(fp, "%4d\n", i + 1 + 1000);
+    }
 
-  fseek(fp, 500 * 5, SEEK_SET);
-  int i, n;
-  for (i = 500; i < 1000; i ++) {
-    fscanf(fp, "%d", &n);
-    assert(n == i + 1);
-  }
+    for (i = 500; i < 1000; i++) {
+        fscanf(fp, "%d", &n);
+        assert(n == i + 1);
+    }
 
-  fseek(fp, 0, SEEK_SET);
-  for (i = 0; i < 500; i ++) {
-    fprintf(fp, "%4d\n", i + 1 + 1000);
-  }
+    fseek(fp, 0, SEEK_SET);
+    for (i = 0; i < 500; i++) {
+        fscanf(fp, "%d", &n);
+        assert(n == i + 1 + 1000);
+    }
 
-  for (i = 500; i < 1000; i ++) {
-    fscanf(fp, "%d", &n);
-    assert(n == i + 1);
-  }
+    fclose(fp);
 
-  fseek(fp, 0, SEEK_SET);
-  for (i = 0; i < 500; i ++) {
-    fscanf(fp, "%d", &n);
-    assert(n == i + 1 + 1000);
-  }
+    printf("PASS!!!\n");
 
-  fclose(fp);
-
-  printf("PASS!!!\n");
-
-  return 0;
+    return 0;
 }
