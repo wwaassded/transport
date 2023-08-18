136633fcda1bd4c00e42478d7acdecebf3184158
tracer-ics2022 <tracer@njuics.org>
2023-08-18 10:07:16 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:07:16 up  1:23,  1 user,  load average: 1.11, 1.12, 0.95

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -4,9 +4,10 @@
 extern intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2);
 int main() {
     write(1, "Hello World!\n", 13);
+    _syscall_(0, 0, 0, 0);
     printf("only test!\n");
     printf("FUCK\n");
-    _syscall_(0, 0, 0, 0);
+
     // assert(0);
     // int i = 2;
     // volatile int j = 0;
