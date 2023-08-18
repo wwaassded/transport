1bf9aee9e2c8cf55aa1b97d42f5382df8fa79301
tracer-ics2022 <tracer@njuics.org>
2023-08-18 10:24:29 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:24:29 up  1:40,  1 user,  load average: 0.18, 0.30, 0.47

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -4,17 +4,13 @@
 extern void _exit(int status);
 int main() {
     write(1, "Hello World!\n", 13);
-    printf("only test!\n");
-    printf("FUCK\n");
-
-    assert(0);
-    // int i = 2;
-    // volatile int j = 0;
-    // while (1) {
-    //     j++;
-    //     if (j == 10000) {
-    //         printf("Hello World from Navy-apps for the %dth time!\n", i++);
-    //         j = 0;
-    //     }
-    // }
+    int i = 2;
+    volatile int j = 0;
+    while (1) {
+        j++;
+        if (j == 10000) {
+            printf("Hello World from Navy-apps for the %dth time!\n", i++);
+            j = 0;
+        }
+    }
 }
