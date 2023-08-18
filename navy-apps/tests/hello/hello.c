81034d5c57531ed2ec4d941b858450339786ce95
tracer-ics2022 <tracer@njuics.org>
2023-08-18 10:14:09 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:14:09 up  1:30,  1 user,  load average: 0.01, 0.35, 0.64

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -1,12 +1,12 @@
 #include <assert.h>
 #include <stdio.h>
 #include <unistd.h>
-extern intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2);
+extern void _exit(int status);
 int main() {
     write(1, "Hello World!\n", 13);
-    _syscall_(0, 0, 0, 0);
-    printf("only test!\n");
-    printf("FUCK\n");
+    _exit(0);
+    // printf("only test!\n");
+    // printf("FUCK\n");
 
     // assert(0);
     // int i = 2;
