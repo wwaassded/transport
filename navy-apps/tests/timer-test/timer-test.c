9cfb3172717dde3bcaf288241e894e73753a9cf4
tracer-ics2022 <tracer@njuics.org>
2023-08-19 09:13:56 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:13:56 up 18:52,  1 user,  load average: 0.09, 0.21, 0.17

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -1,12 +1,14 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
 
+extern uint32_t NDL_GetTicks();
 
 int main() {
     int sec = 0;
     while (1) {
-        while (gettimeofday(NULL, NULL) * 2 < sec)
+        while (NDL_GetTicks() / 1000 < sec)
             ;
         printf("i am a test\n");
         ++sec;
