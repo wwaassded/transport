f4478cf529ae11fa1ed568708e6564fac1f4dd35
tracer-ics2022 <tracer@njuics.org>
2023-08-19 09:48:43 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:48:43 up 19:27,  1 user,  load average: 0.22, 0.19, 0.10

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -6,6 +6,8 @@ typedef size_t (*WriteFn)(const void *buf, size_t offset, size_t len);
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 extern size_t serial_write(const void *buf, size_t offset, size_t len);
+extern size_t events_read(void *buf, size_t offset, size_t len);
+
 typedef struct {
     char *name;
     size_t size;
@@ -18,6 +20,7 @@ typedef struct {
 enum { FD_STDIN,
        FD_STDOUT,
        FD_STDERR,
+       FD_EVENTS,
        FD_FB };
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
@@ -35,7 +38,7 @@ static Finfo file_table[] __attribute__((used)) = {
         [FD_STDIN] = {"stdin", 0, 0, 0, invalid_read, invalid_write},
         [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
         [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},
-        {"/dev/events", 0, 0, 0, invalid_read, invalid_write},
+        [FD_EVENTS] = {"/dev/events", 0, 0, 0, events_read, invalid_write},
 #include "files.h"
 };
 
