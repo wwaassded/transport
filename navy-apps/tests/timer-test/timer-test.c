27478835b3f3fca5ff049a150247b266c7aad332
tracer-ics2022 <tracer@njuics.org>
2023-08-19 09:07:13 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:07:13 up 18:46,  1 user,  load average: 0.57, 0.26, 0.14

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -4,7 +4,12 @@
 
 
 int main() {
-    int res = gettimeofday(NULL, NULL);
-    printf("%d\n", res);
+    int sec = 0;
+    while (1) {
+        while (gettimeofday(NULL, NULL) < sec)
+            ;
+        printf("i am a test\n");
+        ++sec;
+    }
     return 0;
 }
