d5da2a076897d3c096dc2dfb9d49c93a136549b7
tracer-ics2022 <tracer@njuics.org>
2023-08-19 17:41:45 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:41:45 up 53 min,  1 user,  load average: 0.23, 0.16, 0.06

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -7,8 +7,8 @@
 int main() {
     NDL_Init(0);
     int w, h;
-    assert(0);
     void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
+    assert(0);
     assert(bmp);
     NDL_OpenCanvas(&w, &h);
     NDL_DrawRect(bmp, 0, 0, w, h);
