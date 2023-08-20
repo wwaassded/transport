3ddab6fc027fa39f8bd986cb2f1799b9c298c65c
tracer-ics2022 <tracer@njuics.org>
2023-08-20 09:55:54 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:55:54 up 2 min,  1 user,  load average: 0.59, 0.25, 0.10

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -39,7 +39,8 @@ static Finfo file_table[] __attribute__((used)) = {
         [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
         [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},
         [FD_EVENTS] = {"/dev/events", 0, 0, 0, events_read, invalid_write},
-        {"/proc/dispinfo", 0, 0, 0, dispinfo_read, invalid_write},
+        // {"/proc/dispinfo", 0, 0, 0, dispinfo_read, invalid_write},
+        {"/proc/dispinfo", 0, 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
 
