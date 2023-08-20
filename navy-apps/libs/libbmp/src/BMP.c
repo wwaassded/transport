28d6ca83866cecd57bf6e8bea501e7f3d95aa22f
tracer-ics2022 <tracer@njuics.org>
2023-08-20 10:17:54 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:17:54 up 24 min,  3 users,  load average: 0.39, 0.24, 0.14

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -35,12 +35,12 @@ void *BMP_Load(const char *filename, int *width, int *height) {
     int line_off = (w * 3 + 3) & ~0x3;
     for (int i = 0; i < h; i++) {
         fseek(fp, hdr.offset + (h - 1 - i) * line_off, SEEK_SET);
-        // int nread = fread(&pixels[w * i], 3, w, fp);
+        int nread = fread(&pixels[w * i], 3, w, fp);
         for (int j = w - 1; j >= 0; j--) {
             uint8_t b = *(((uint8_t *) &pixels[w * i]) + 3 * j);
             uint8_t g = *(((uint8_t *) &pixels[w * i]) + 3 * j + 1);
             uint8_t r = *(((uint8_t *) &pixels[w * i]) + 3 * j + 2);
-            pixels[w * i + j] = (r << 16) | (g << 8) | b;
+            // pixels[w * i + j] = (r << 16) | (g << 8) | b;
         }
     }
     printf("TEST\n");
