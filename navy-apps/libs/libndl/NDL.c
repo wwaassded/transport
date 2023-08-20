439ff5076a5bccf62716b352f2e891c0473037da
tracer-ics2022 <tracer@njuics.org>
2023-08-20 18:29:19 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:29:19 up  1:49,  3 users,  load average: 0.38, 0.33, 0.28

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -46,7 +46,7 @@ void NDL_OpenCanvas(int *w, int *h) {
         read(4, buf, 64);
         char *tmp = strtok(buf, ":");
         char *number = strtok(NULL, " ");
-        printf("%s %s\n", tmp, number);
+        printf("%s", tmp);
         if (strcmp(tmp, "WIDTH") == 0)
             *w = atoi(number);
         else if (strcmp(tmp, "HEIGHT") == 0)
