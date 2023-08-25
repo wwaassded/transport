0f83d9cf07ae41d16cc053f3876bc6f7fb74473a
tracer-ics2022 <tracer@njuics.org>
2023-08-25 08:33:15 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:33:15 up  3:54,  1 user,  load average: 0.05, 0.11, 0.08

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -146,6 +146,7 @@ static void draw_ch(BDF_Font *font, int x, int y, char ch, uint32_t fg, uint32_t
     SDL_Surface *s = BDF_CreateSurface(font, ch, fg, bg);
     SDL_Rect dstrect = {.x = x, .y = y};
     SDL_BlitSurface(s, NULL, screen, &dstrect);
+    assert(0);
     SDL_FreeSurface(s);
 }
 
