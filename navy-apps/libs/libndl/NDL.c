eece3a198a8e275436a1006b6f1977f8c9f74000
tracer-ics2022 <tracer@njuics.org>
2023-08-28 20:43:59 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:43:59 up 11:23,  1 user,  load average: 0.64, 0.59, 0.33

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -12,7 +12,7 @@ static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
 
 uint32_t NDL_GetTicks() {
-    return gettimeofday(NULL, NULL) * 100;
+    return gettimeofday(NULL, NULL) * 10000;
 }
 
 int NDL_PollEvent(char *buf, int len) {
