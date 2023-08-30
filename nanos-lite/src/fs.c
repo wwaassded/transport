4a478128406b8226f626f4ea0e700ef73d99b808
tracer-ics2022 <tracer@njuics.org>
2023-08-30 16:01:23 +0800

    >  compile NEMU 211220000 张三 Linux what 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:01:23 up  1:23,  1 user,  load average: 0.15, 0.42, 0.41

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -86,7 +86,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
     }
     size_t ret = ramdisk_read(buf, file_table[fd].open_offset, len);
     file_table[fd].open_offset += len;
-    assert(0);
+    assert(ret);
     return ret;
 }
 
